f6ee25577c3fc89ffea1d8f5420ae57f3b61bd27
Ebuka Ejiofor <ebuskelele@gmail.com>
2023-01-29 19:59:50 -0800

    add useEffect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
+	const [name, setName] = useState("mario");
 	const [blogs, setBlogs] = useState([
 		{
 			title: "My new Website",
@@ -18,18 +19,34 @@ const Home = () => {
 		{
 			title: "Web Dev Top Tips",
 			body: "lorem ipsum dolor...",
-			author: "Luigi",
+			author: "Mario",
 			id: 3,
 		},
 	]);
 
-	const handleClick = () => {
-		console.log("ebuka is awesome");
+	const handleDelete = (id) => {
+		const newBlogs = blogs.filter((blog) => blog.id !== id);
+		setBlogs(newBlogs);
 	};
 
+	useEffect(() => {
+		console.log("use effect ran");
+		console.log(name);
+	}, [ name ]);
+	
 	return (
 		<div>
-			<BlogList blogs={blogs} />
+			<BlogList
+				blogs={blogs}
+				title="All Blogs"
+				handleDelete={handleDelete}
+			/>
+			<button
+				className="bg-slate-300 px-2 py-1 text-black"
+				onClick={() => setName("luigi")}
+			>
+				Change name
+			</button>
 		</div>
 	);
 };
